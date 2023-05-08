2d3b2b6642d3f021c37bc8e191916e4afe08dafb
Rosie Mark <rosiemark.ja@gmail.com>
2023-05-08 22:10:49 +0300

    first commit

---
+++ firstapp/src/Nav/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './Nav.css';
+
+
+function Nav() {
+  return (
+    <div>
+        <h3>Adding a menu 🙂</h3>
+        <nav>
+            <a href="" target="_blank">Home</a>
+            <a href="" target="_blank">About</a>
+            <a href="" target="_blank">Investor Relations</a>
+            <a href="" target="_blank">Our Clients</a>
+            <a href="" target="_blank">Media</a>
+            <a href="" target="_blank">Contact Us</a>
+        </nav>
+        <div class="space"></div>
+    </div>
+  )
+}
+
+export default Nav
